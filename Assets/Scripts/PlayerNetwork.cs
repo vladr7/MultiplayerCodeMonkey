c01c50f72275dd35dbb91975f0826b31aa82b0a8
Vlad <ricean.vlad7@gmail.com>
2023-12-12 19:41:54 +0200

    Network variable working (permissions for Everyone, Owner, Server)

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -6,29 +6,46 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    private NetworkVariable<int> score = new NetworkVariable<int>(
+        1,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner
+    );
+
     private void Update()
     {
+        Debug.Log(OwnerClientId + " ; score: " + score.Value);
         if (!IsOwner)
         {
             return;
         }
+
         Vector3 moveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W))
         {
             moveDir += Vector3.forward;
         }
+
         if (Input.GetKey(KeyCode.S))
         {
             moveDir += Vector3.back;
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             moveDir += Vector3.left;
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             moveDir += Vector3.right;
         }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            score.Value++;
+        }
+
         float moveSpeed = 3f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
