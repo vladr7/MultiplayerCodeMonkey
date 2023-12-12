519fa042c5a5a72f12eec36101c003e7964d8e55
Vlad <ricean.vlad7@gmail.com>
2023-12-12 19:11:26 +0200

    Only move the player that is the owner of the Player instance

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
     private void Update()
     {
+        if (!IsOwner)
+        {
+            return;
+        }
         Vector3 moveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W))
         {
